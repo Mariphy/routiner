a96777e44b0e0bbbe7c2f3e7c04e102dd047afe0
Maria <ev.maria.pro@gmail.com>
2025-04-09 11:24:26 -0400

    bcrypt helpers

--- routiner/src/app/utils/bcrypt.ts
+++ routiner/src/app/utils/bcrypt.ts
@@ -1,5 +1,9 @@
 import bcrypt from "bcryptjs";
 
+export async function hashPassword(password: string) {
+  return bcrypt.hash(password, 12);
+}
+
 export async function verifyPassword(inputPassword: string, hashedPassword: string) {
   return bcrypt.compare(inputPassword, hashedPassword);
 }
