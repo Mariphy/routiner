70bf7df09c63f0d21d83936dfad55f412ca2447b
Maria <ev.maria.pro@gmail.com>
2025-04-24 16:50:27 -0400

    edit NavBar styling

--- routiner/src/app/components/NavBar.tsx
+++ routiner/src/app/components/NavBar.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 export default function NavBar() {
     return (
-        <nav className="flex-row justify-between items-stretch p-4 bg-accent text-white">
+        <nav className="flex-row justify-between items-stretch p-4 bg-accent text-neutral-100">
             <h1 className="text-2xl">Routiner App</h1>
             <div className="flex space-x-4">
-                <Link className="text-white m-4" href="/"> Home </Link>
-                <Link className="text-white m-4" href="/board"> Board </Link>
-                <Link className="text-white m-4" href="/calendar"> Calendar </Link>
+                <Link className="text-neutral-100 m-4" href="/"> Home </Link>
+                <Link className="text-neutral-100e m-4" href="/board"> Board </Link>
+                <Link className="text-neutral-100 m-4" href="/calendar"> Calendar </Link>
             </div>
         </nav>
     );
