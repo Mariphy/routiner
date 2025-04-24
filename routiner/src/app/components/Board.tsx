4258eb28f7136f557df44b02fee5fc4ffafb085e
Maria <ev.maria.pro@gmail.com>
2025-04-24 15:46:07 -0400

    update styling

--- routiner/src/app/components/Board.tsx
+++ routiner/src/app/components/Board.tsx
@@ -25,9 +25,9 @@ export default function Board({ tasks, onAddTask }: BoardProps) {
   };
 
   return (
-    <div className="board-container overflow-x-auto bg-gray-100">
-      <div className="board flex gap-1 p-4 min-w-max min-h-[600px]">
-        <div className="column border p-4 w-full sm:w-64 md:w-72 lg:w-80 rounded-lg bg-white shadow-md">
+    <div className="board-container w-full overflow-x-auto scroll-px-4">
+      <div className="board flex gap-2 p-4 min-w-fit min-h-[600px]">
+        <div className="column border p-4 sm:w-64 md:w-72 lg:w-80 flex-shrink-0 rounded-lg bg-gray-100 shadow-md">
           <h2 className="text-xl font-bold mb-4">Task List</h2>
           {tasks.map((task, index) => (
             <div key={index} className="task border p-2 mb-2">
@@ -44,7 +44,7 @@ export default function Board({ tasks, onAddTask }: BoardProps) {
             />
             <button
               onClick={() => handleAddTask()}
-              className="bg-blue-500 text-white px-4 py-2 rounded"
+              className="bg-accent text-white px-4 py-2 rounded"
             >
               +
             </button>
@@ -54,7 +54,7 @@ export default function Board({ tasks, onAddTask }: BoardProps) {
         {daysOfWeek.map((day, index) => {
           const dayName = format(day, 'EEEE'); // Get the full name of the day (e.g., "Monday")
           return (
-            <div key={index} className="column border p-4 w-full sm:w-64 md:w-72 lg:w-80 rounded-lg bg-base shadow-md">
+            <div key={index} className="column border p-4 sm:w-64 md:w-72 lg:w-80 rounded-lg bg-gray-100 shadow-md">
               <h2 className="text-xl font-bold mb-4">{dayName}</h2>
               {tasks
                 .filter((task) => task.day === dayName) // Filter tasks for the current day
--- routiner/src/app/components/NavBar.tsx
+++ routiner/src/app/components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NavBar() {
     return (
-        <nav className="flex-row justify-between items-stretch p-4 bg-gray-800 text-white">
+        <nav className="flex-row justify-between items-stretch p-4 bg-accent text-white">
             <h1 className="text-2xl">Routiner App</h1>
             <div className="flex space-x-4">
                 <Link className="text-white m-4" href="/"> Home </Link>
