ee4c7821099d05eca66fe4ec87b20ef67f6cab6e
Maria <ev.maria.pro@gmail.com>
2025-04-27 17:03:09 -0400

    edit NavBar, delete home link

--- routiner/src/app/components/NavBar.tsx
+++ routiner/src/app/components/NavBar.tsx
@@ -5,7 +5,6 @@ export default function NavBar() {
         <nav className="flex items-center justify-between p-4 bg-accent text-neutral-100 shadow-sm">
             <Link href="/" className="text-2xl font-semibold">Routiner</Link>
             <div className="flex space-x-6 text-sm">
-                <Link href="/" className="text-xl hover:underline">Home</Link>
                 <Link href="/board" className="text-xl hover:underline">Board</Link>
                 <Link href="/calendar" className="text-xl hover:underline">Calendar</Link>
             </div>
