1d8e0328fbcd0402c5152b089d7843e824a4ba50
Maria <ev.maria.pro@gmail.com>
2025-05-13 15:57:19 -0400

    add flowbite

--- routiner/src/app/layout.tsx
+++ routiner/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
+import { ThemeModeScript } from 'flowbite-react'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <head>
+          <ThemeModeScript />
+       </head>
        <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- routiner/tailwind.config.ts
+++ routiner/tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import flowbiteReact from "flowbite-react/plugin/tailwindcss";
 
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ".flowbite-react\\class-list.json"
   ],
   theme: {
     extend: {
@@ -32,5 +34,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [flowbiteReact],
 } satisfies Config;
