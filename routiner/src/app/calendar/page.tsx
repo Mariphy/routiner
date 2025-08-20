c0db5ea5a6d6b597e8bd94f51374f808a4b90dfa
Maria <ev.maria.pro@gmail.com>
2025-08-19 21:33:14 -0400

    convert calendar page into the server component

--- routiner/src/app/calendar/page.tsx
+++ routiner/src/app/calendar/page.tsx
@@ -1,28 +1,16 @@
-"use client";
-
 import Calendar from "../components/Calendar";
-import type {Event as CalendarEvent} from '@/app/types.ts'
-import { useEffect, useState } from 'react';
 import { fetchUserId, getEventsByMonth } from '@/app/lib/api'; // or your fetch function
 
-export default function CalendarPage() {
-  const [events, setEvents] = useState<CalendarEvent[]>([]);
-
-  useEffect(() => {
-    async function fetchEvents() {
-      const userId = await fetchUserId();
-      const month = new Date().toISOString().slice(0, 7); // 'YYYY-MM'
-      const eventsData = await getEventsByMonth(userId, month);
-      setEvents(eventsData);
-    }
-    fetchEvents();
-  }, []);
-    return (
-      <main className="flex-grow flex flex-col sm:flex-row pt-12">
-        <div className="flex-1 p-4">
-          {<Calendar events = {events} />}
-          
-        </div>
-      </main>
-    );
+export default async function CalendarPage() {
+  const userId = await fetchUserId();
+  const month = new Date().toISOString().slice(0, 7); // 'YYYY-MM'
+  const events = await getEventsByMonth(userId, month);
+     
+  return (
+    <main className="flex-grow flex flex-col sm:flex-row pt-12">
+      <div className="flex-1 p-4">
+        {<Calendar events={events} />}
+      </div>
+    </main>
+  );
 }
