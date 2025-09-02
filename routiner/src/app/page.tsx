0ce20dc3b7e5e129f45f8fdb6f8576a4c23b4871
Maria <ev.maria.pro@gmail.com>
2025-09-02 13:42:47 -0400

    update home page

--- routiner/src/app/page.tsx
+++ routiner/src/app/page.tsx
@@ -4,18 +4,23 @@ import UserHomePage from "./components/UserHomePage";
 
 export default async function Home() {
   const session = await auth();
+  if (!session || !session.user) {
+    return (
+      <main className="flex-grow flex flex-col sm:flex-row items-center justify-center pt-12">
+        <div className="m-6">
+          <GuestHomePage />
+        </div>
+      </main>
+    )
+  }
 
   return (
     <main className="flex-grow flex flex-col sm:flex-row items-center justify-center pt-12">
       <div className="m-6">
-        {session ? ( 
-          <div className="text-center m-6">
-            <h1 className="text-2xl font-bold">Welcome, {session.user.name}!</h1>
-            <UserHomePage />
-          </div>  
-        ) : ( 
-          <GuestHomePage />
-        )}
+        <div className="text-center m-6">
+          <h1 className="text-2xl font-bold">Welcome, {session.user.name}!</h1>
+          <UserHomePage />
+        </div>  
       </div>
     </main>
   );
