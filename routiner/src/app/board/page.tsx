8c11db751fa13fe26654aa63e6773a6cadd65c41
Maria <ev.maria.pro@gmail.com>
2025-09-08 12:38:29 -0400

    format file

--- routiner/src/app/board/page.tsx
+++ routiner/src/app/board/page.tsx
@@ -1,7 +1,7 @@
 "use server";
 import Board from '../components/Board';
 import AddButton from '../components/AddButton';
-import {  getEventsForCurrentWeek, getRoutines, getTasks } from '../lib/api';
+import { getEventsForCurrentWeek, getRoutines, getTasks } from '../lib/api';
 import { fetchUserIdServer } from '@/app/lib/actions/userId';
 //import { preloadBoardData } from '../lib/preload';
 import type { Routine, Task, Event } from '@/app/types';
@@ -19,30 +19,30 @@ export default async function BoardPage() {
   if (userId) {
     // Start preloading data
     //preloadBoardData();
-    
+
     try {
       const [tasksData, routinesData, eventsData] = await Promise.all([
-        getTasks(userId, {cookies: cookieStore, headers: allHeaders}),
-        getRoutines(userId, {cookies: cookieStore, headers: allHeaders}),
-        getEventsForCurrentWeek(userId, {cookies: cookieStore, headers: allHeaders}),
+        getTasks(userId, { cookies: cookieStore, headers: allHeaders }),
+        getRoutines(userId, { cookies: cookieStore, headers: allHeaders }),
+        getEventsForCurrentWeek(userId, { cookies: cookieStore, headers: allHeaders }),
       ]);
-      
+
       tasks = tasksData.tasks;
       routines = routinesData.routines;
       events = eventsData;
     } catch (error) {
       console.error('Error fetching data:', error);
     }
-  }  
+  }
 
   return (
     <main className="flex-grow flex flex-col sm:flex-row pt-12 mt-4">
       <div className="flex-1 p-4">
-        <Board 
-          tasks={tasks} 
+        <Board
+          userId={userId}
+          tasks={tasks}
           routines={routines}
           events={events}
-          userId={userId}
         />
         <AddButton />
       </div>
