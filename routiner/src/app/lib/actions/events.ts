ab71f7ad9eeea3cf5a4e87509f5a6bb7ad78ab0e
Maria <ev.maria.pro@gmail.com>
2025-09-09 19:05:10 -0400

    add server action to add events

--- routiner/src/app/lib/actions/events.ts
+++ routiner/src/app/lib/actions/events.ts
@@ -1,1 +1,56 @@
-//to-do move addEvents, editEvents, deleteEvents here
+"use server";
+
+import { revalidatePath } from 'next/cache';
+import { connectToDb } from '@/app/api/db';
+import type { Event, UserDocument } from '@/app/types';
+import { generateUniqueId } from '@/app/utils/helpers';
+import { auth } from "@/auth";
+
+export async function addEvent(formData: FormData) {
+  try {
+    const session = await auth();
+    if (!session?.user?.email) {
+      return { success: false, error: 'Unauthenticated' };
+    }
+
+    const rawTitle = (formData.get('title') ?? '').toString().trim();
+    if (!rawTitle) {
+      return { success: false, error: 'Task title is required' };
+    }
+    const dateRaw = (formData.get('date') ?? '').toString();
+    if (!dateRaw) {
+      return { success: false, error: 'Event date is required' };
+    }
+    const startTimeRaw = (formData.get('startTime') ?? '').toString();
+    if (!startTimeRaw) {
+      return { success: false, error: 'Start time is required' };
+    }
+    const dateTimeString = `${dateRaw}T${startTimeRaw}:00`;
+    const date = new Date(dateTimeString);
+
+    const event: Event = {
+      id: generateUniqueId(),
+      title: rawTitle,
+      date: date,
+      startTime: startTimeRaw,
+      endTime: formData.get('endTime') as string,
+    };
+
+    const { db } = await connectToDb();
+    const result = await db.collection<UserDocument>('users').updateOne(
+      { email: session.user.email },
+      { $push: { events: event } }
+    );
+
+    if (result.modifiedCount === 0) {
+      return { success: false, error: 'Failed to add task' };
+    }
+
+    revalidatePath('/board');
+    revalidatePath('/calendar');
+    return { success: true, event };
+  } catch (error) {
+    console.error(error);
+    return { success: false, error: 'Internal error' };
+  }
+}
