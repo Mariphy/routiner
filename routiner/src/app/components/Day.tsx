dd1a06eed5403dfdc253f07d87ea909cfeaba639
Maria <ev.maria.pro@gmail.com>
2025-10-13 13:46:32 -0400

    Pass tasks to the component, move fetching logic

--- routiner/src/app/components/Day.tsx
+++ routiner/src/app/components/Day.tsx
@@ -1,32 +1,18 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import type { Event as EventType, Task as TaskType } from '@/app/types';
 import Event from "@/app/components/Event";
 import Task from "@/app/components/Task";
-import { getTasksByDate } from '@/app/lib/api';
+
 
 interface DayProps {
     userId: string;
     selectedDate: Date;
     events: EventType[];
+    tasks: TaskType[];
 }
 
-export default function Day({ userId, selectedDate, events }: DayProps) {
-    const [tasks, setTasks] = useState<TaskType[]>([]);
-    
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const dateString = selectedDate.toISOString().split('T')[0];
-                const fetchedTasks = await getTasksByDate(userId, dateString);
-                setTasks(fetchedTasks);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        }
-        fetchData();
-    }, [selectedDate, userId]);
-
+export default function Day({ userId, selectedDate, events, tasks }: DayProps) { 
     return (
         <div className='pb-10'>
             <h2 className="text-2xl font-bold mb-4 text-center">Shedule for the day</h2>
