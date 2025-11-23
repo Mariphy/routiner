58a2ea2408b60de534e3b7f0aabbdd50536d3461
maria <ev.maria.pro@gmail.com>
2025-11-22 21:49:14 -0500

    refactor data passed to edit task action

--- routiner/src/app/components/Task.tsx
+++ routiner/src/app/components/Task.tsx
@@ -32,10 +32,6 @@ export default function Task({ task }: TaskProps) {
     formData.append('title', task.title);
     formData.append('checked', e.target.checked ? 'on' : '');
 
-    if (task.day) formData.append('day', task.day);
-    if (task.date) formData.append('date', task.date.toISOString());
-    if (task.startTime) formData.append('startTime', task.startTime);
-    if (task.endTime) formData.append('endTime', task.endTime);
     startTransition(async () => {
       try {
         const result = await editTask(formData, task.id);
